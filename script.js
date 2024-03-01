0dd1d45df44a8e5b3029613c5772473333e11c3f
anirbansaha2002 <98881271+anirbansaha2002@users.noreply.github.com>
2024-03-01 17:00:53 +0530

    Add files via upload
    Added JavaScript

---
+++ script.js
@@ -0,0 +1,15 @@
+
+function toggleDropDownServices(){
+    let dropdownServices = document.querySelector('#services-drop-down-list');
+    dropdownServices.classList.toggle('hidden')
+}
+
+function toggleDropDownBlogs(){
+    let dropdownBlogs = document.querySelector('#blogs-drop-down-list');
+    dropdownBlogs.classList.toggle('hidden')
+}
+
+function toggleDropDownLocations(){
+    let dropdownLocations = document.querySelector('#locations-drop-down-list');
+    dropdownLocations.classList.toggle('hidden')
+}
